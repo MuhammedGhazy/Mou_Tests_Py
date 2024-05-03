bf895e09b137881dc6da8f8c85066ca3405d88e5
Muhammed <muhammedghazy.com>
2024-05-04 01:07:08 +0300

    py

---
+++ 0x01/6_main.py
@@ -0,0 +1,12 @@
+from pystyle import *
+import emoji
+
+print(Box.Lines("[+] Learn Python With Muhammed  [+]"))
+Write.Print("This Programe For Use Emoji \n", Colors.blue_to_red, 0.1)
+print(Box.DoubleCube("Example 7"))
+print(emoji.emojize ("I Love Python :yellow_heart:"))
+print(emoji.emojize ("I Love C :blue_heart:"))
+print(emoji.emojize ("I Love C++ :red_heart:"))
+print(emoji.emojize ("I Love Php :purple_heart:"))
+print(emoji.emojize ("I Love Java :green_heart:"))
+print(emoji.emojize("Muhammed :broken_heart:"))
