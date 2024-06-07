261d27258a99e7b52c6e65bf357b5cfb749dba96
Mou <mou@yahoo.com>
2024-06-07 15:15:54 +0300

    del

--- django/project/pages/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Female, Male, User,Product, Viedo, User_1
-# Register your models here.
-admin.site.register(Female)
-admin.site.register(Male)
-admin.site.register(User)
-admin.site.register(Product)
-admin.site.register(Viedo)
-admin.site.register(User_1)
