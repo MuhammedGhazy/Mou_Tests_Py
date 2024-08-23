b6d3b914c429775a1eb33b19f8ef03bc9c6ce8cb
Mou <141512852+MuhammedGhazy@users.noreply.github.com>
2024-08-23 10:46:48 -0500

    Delete Django/Cinema_Tickets-reservation_API directory
    Edited in new repo

--- Django/Cinema_Tickets-reservation_API/tickets/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Guest, Movie, Reservations
-# Register your models here.
-admin.site.register(Guest)
-admin.site.register(Movie)
-admin.site.register(Reservations)
